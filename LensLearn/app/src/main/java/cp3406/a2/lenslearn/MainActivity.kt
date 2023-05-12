733c62f98510cdc8f55965bfe8e9e5634fac0453
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-13 03:32:51 +1000

    Add transition from landing to activity

--- LensLearn/app/src/main/java/cp3406/a2/lenslearn/MainActivity.kt
+++ LensLearn/app/src/main/java/cp3406/a2/lenslearn/MainActivity.kt
@@ -1,13 +1,30 @@
 package cp3406.a2.lenslearn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import cp3406.a2.lenslearn.view.CategoryActivity
 
 class MainActivity : AppCompatActivity() {
+
+    /** On Create Inflate Layout and Set Click Listener */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playButton = findViewById<Button>(R.id.playButton)
+        playButton.setOnClickListener {
+            val intent = Intent(this, CategoryActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_out, R.anim.fade_in)
+        }
     }
 
-
+    /** On Pause Continue Transition */
+    override fun onPause() {
+        super.onPause()
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+    }
 }
+
--- LensLearn/app/src/main/java/cp3406/a2/lenslearn/view/CategoryActivity.kt
+++ LensLearn/app/src/main/java/cp3406/a2/lenslearn/view/CategoryActivity.kt
@@ -1,11 +1,13 @@
-package cp3406.a2.lenslearn
+package cp3406.a2.lenslearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cp3406.a2.lenslearn.R
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val binding = DataBindingUtil.setContentView<ActivityCategoryBinding>(this, R.layout.activity_category)
         setContentView(R.layout.activity_category)
     }
 }
