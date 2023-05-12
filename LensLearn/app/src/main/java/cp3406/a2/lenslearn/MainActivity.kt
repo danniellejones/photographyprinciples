f1a140acdfd0815359c0fe84f124d2d9daf98e4f
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-12 22:12:03 +1000

    Add dependencies

---
+++ LensLearn/app/src/main/java/cp3406/a2/lenslearn/MainActivity.kt
@@ -0,0 +1,13 @@
+package cp3406.a2.lenslearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ LensLearner/app/src/main/java/cp3406/a2/lenslearner/MainActivity.kt
@@ -0,0 +1,11 @@
+package cp3406.a2.lenslearner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
