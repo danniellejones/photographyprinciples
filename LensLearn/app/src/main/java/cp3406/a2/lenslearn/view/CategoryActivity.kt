21f14fc8fc6ccca72ffb312254d077a0df667b36
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-13 12:19:54 +1000

    Refactor activity category layouts

--- LensLearn/app/src/main/java/cp3406/a2/lenslearn/view/CategoryActivity.kt
+++ LensLearn/app/src/main/java/cp3406/a2/lenslearn/view/CategoryActivity.kt
@@ -2,12 +2,16 @@ package cp3406.a2.lenslearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import cp3406.a2.lenslearn.R
+import cp3406.a2.lenslearn.databinding.ActivityCategoryBinding
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val binding = DataBindingUtil.setContentView<ActivityCategoryBinding>(this, R.layout.activity_category)
-        setContentView(R.layout.activity_category)
+        val binding = DataBindingUtil.setContentView<ActivityCategoryBinding>(this, R.layout.activity_category)
+//        setContentView(R.layout.activity_category)
     }
+
+
 }
